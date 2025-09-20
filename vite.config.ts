74d38b1ce87ad66c069439778b84cd7ffa3950ca
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-09-20 16:12:01 +0300

    chore: uptade vite configue and add vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, ConfigEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::", 
-    port: 8080,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 10000, // Render fournit PORT
-    allowedHosts: ["honey-bloom-frontend.onrender.com"],
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const port = Number(process.env.PORT) || 10000;
+
+  return {
+    server: {
+      host: "::",
+      port: 8080, 
     },
-  },
-}));
+    preview: {
+      host: "0.0.0.0", 
+      port, 
+      allowedHosts: ["honey-bloom-frontend.onrender.com"],
+    },
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
